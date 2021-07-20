3d18ff08cb86a777a29bbb4cd60d453c99b15cc0
gelargew <gelargew@gmail.com>
2021-07-20 09:45:51 +0700

    camera

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,26 @@
 import Head from 'next/head'
 import Layout from '../components/Layout'
 import { MainContainer } from '../components/commons'
-import { Canvas } from '@react-three/fiber'
+import { Canvas, PerspectiveCameraProps, ThreeEvent, useFrame } from '@react-three/fiber'
 import { useState, useEffect, useRef } from 'react'
-import { Box, CameraShake, MapControls, OrbitControls, OrthographicCamera, PerspectiveCamera, PointerLockControls } from '@react-three/drei'
-import { AmbientLight, Mesh, Object3D, Vector3 } from 'three'
+import { Box, CameraShake, MapControls, OrbitControls, PerspectiveCamera, Plane } from '@react-three/drei'
+import * as THREE from 'three'
 
 
 
 export default function Home() {
-  const [pointer, setPointer] = useState<Vector3>(new Vector3(0, 0, 0))
+  const [pointer, setPointer] = useState<THREE.Vector3>(new THREE.Vector3(0, 0, 4))
+  const [distance, setDistance] = useState(0)
+  const cameraRef = useRef<THREE.PerspectiveCamera>(null!)
+
+  const handlePointerMove = (e:ThreeEvent<PointerEvent>) => {
+    setPointer(prev => {
+      prev.setX(e.point.x)
+      prev.setY(e.point.y)
+      return prev
+    })
+    setDistance(e.distance)
+  }
 
   return (
     <Layout>
@@ -20,12 +31,23 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
         </Head>
       <MainContainer>
-        <Canvas>
-          <mesh onClick={e => console.log(e)}>
-
-          
+        <Canvas camera={cameraRef.current} >
+          <PerspectiveCamera ref={cameraRef} makeDefault position={[0, 0, 10]} />
+          <OrbitControls camera={cameraRef.current}  />
+          <mesh  >
+            <Plane args={[80, 80, 20, 20]}  position={[0, 0, -4]}>
+                  <meshPhongMaterial attach='material' wireframe/>
+            </Plane>
+            
+            <Plane args={[80, 80, 1, 1]}  position={[0, 0, 5]} onClick={e => cameraRef.current.translateOnAxis(pointer, 2) } onPointerMove={handlePointerMove}>
+                  <meshPhongMaterial wireframe transparent />
+            </Plane>
             <CameraMovement pointer={pointer} />
+          
+            
+            
           </mesh>
+
         </Canvas>
       </MainContainer>
         
@@ -34,19 +56,24 @@ export default function Home() {
 }
 
 
-const CameraMovement = ({pointer}: { pointer: Vector3}) => {
-  const boxRef = useRef<Object3D>(null!)
-  useEffect(() => {
-    boxRef.current.lookAt(pointer)
-  }, [pointer])
+const CameraMovement = ({pointer}: { pointer: THREE.Vector3}) => {
+  const boxRef = useRef<THREE.Object3D>(null!)
+  const [prevQt, setPrevQt] = useState<THREE.Vector3>(new THREE.Vector3(0, 0, 100))
+  
+
+  useFrame(() => {
+    const qt = new THREE.Vector3().lerpVectors(prevQt, pointer, 0.1)
+    boxRef.current.lookAt(qt)
+    setPrevQt(qt)
+  })
   return (
     <>
     
       <Box ref={boxRef} args={[5, 5, 5]} position={[0, 0, -4]} onClick={() => console.log(pointer)}>
-        <meshPhongMaterial wireframe />
+        <meshPhongMaterial color='blue' wireframe />
       </Box>
       <ambientLight intensity={0.5} />
-      <MapControls />
+      
       
       
     </>
